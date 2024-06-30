a4ce42ac39693d7d8e8030e4312d8d0afb395708
SwapnilSahare45 <swapnilsahare45@gmail.com>
2024-06-30 13:05:42 +0530

    Some Files added

--- contact.php
+++ contact.php
@@ -1,44 +1,29 @@
-<!doctype html>
-<html lang="en">
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Contact US</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link rel="stylesheet" href="style/style.css">
-</head>
-
-<body>
-    <div class="row justify-content-evenly mb-5 " id="contact">
-
-        <div class="col-lg-10 shadow-lg rounded my-3 px-5 py-4">
-            <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post">
-                <div class="col-lg-12">
-                    <h1 class="text-center fw-bold mt-2 mb-4" style="font-size: 4.6rem;">Contact Us</h1>
-                </div>
-                <div class="mb-3">
-                    <label for="username" class="form-label">Name</label>
-                    <input type="text" class="form-control" id="username" name="username" placeholder="Enter your full name">
-                </div>
-                <div class="mb-3">
-                    <label for="useremail" class="form-label">Email</label>
-                    <input type="text" class="form-control" id="useremail" name="useremail" placeholder="Enter your email">
-                </div>
-                <div class="mb-3">
-                    <label for="usermsg" class="form-label">Message</label>
-                    <textarea class="form-control" id="usermsg" rows="3" name="usermsg" placeholder="Enter your message"></textarea>
-                </div>
-                <div class="col-auto">
-                    <button type="submit" class="btn btn-primary">Send</button>
-                </div>
-            </form>
-        </div>
+<div class="row justify-content-evenly mb-5" id="contact">
+    <div class="col-lg-10 shadow-lg rounded my-3 px-5 py-4">
+        <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post">
+            <div class="col-lg-12">
+                <h1 class="text-center fw-bold mt-3 mb-4 fs-1" >Contact Us</h1>
+            </div>
+            <div class="mb-3">
+                <label for="username" class="form-label">Name</label>
+                <input type="text" class="form-control" id="username" name="username" placeholder="Enter your full name">
+            </div>
+            <div class="mb-3">
+                <label for="useremail" class="form-label">Email</label>
+                <input type="text" class="form-control" id="useremail" name="useremail" placeholder="Enter your email">
+            </div>
+            <div class="mb-3">
+                <label for="usermsg" class="form-label">Message</label>
+                <textarea class="form-control" id="usermsg" rows="3" name="usermsg" placeholder="Enter your message"></textarea>
+            </div>
+            <div class="col-auto mt-4 mb-2">
+                <button type="submit" class="btn btn-primary">Send</button>
+            </div>
+        </form>
     </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-</body>
+</div>
 
-</html>
 <?php
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
     require "connection.php";
--- index.php
+++ index.php
@@ -93,9 +93,9 @@ $username = $_SESSION['username'];
     </div>
 
     <?php
-    include 'about.php';
+    include 'about.html';
     include 'contact.php';
-    include 'footer.php'
+    include 'footer.html';
     ?>
   </div>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
